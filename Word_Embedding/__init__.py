dd7b8883747182a9772e27e2e3a22747555335e3
andrea824 <18072836885@163.com>
2019-01-15 07:28:26 -0500

    add embedding file

---
+++ Word_Embedding/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19-1-15 上午6:41
+# @Author  : zheng
+# @Software: PyCharm
---
+++ Word_Embedding/skip_gram.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19-1-15 上午6:42
+# @Author  : zheng
+# @Software: PyCharm
+
